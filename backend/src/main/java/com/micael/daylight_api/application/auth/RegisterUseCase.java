3a3325e8cd6c0b88f604c46332182accee4dba7b
iSantanaaH <micaelsantana.dev@gmail.com>
2026-02-22 16:37:30 -0300

    create hash from user password

--- backend/src/main/java/com/micael/daylight_api/application/auth/RegisterUseCase.java
+++ backend/src/main/java/com/micael/daylight_api/application/auth/RegisterUseCase.java
@@ -3,6 +3,7 @@ package com.micael.daylight_api.application.auth;
 import com.micael.daylight_api.domain.enums.Role;
 import com.micael.daylight_api.domain.model.User;
 import com.micael.daylight_api.domain.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,9 +11,11 @@ import java.time.LocalDate;
 @Service
 public class RegisterUseCase {
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
-	public RegisterUseCase(UserRepository userRepository) {
+	public RegisterUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	public String register(RegisterRequest registerRequest) {
@@ -21,11 +24,12 @@ public class RegisterUseCase {
 			throw new RuntimeException("Email already exists");
 		}
 
+		String encodedPassword = passwordEncoder.encode(registerRequest.getPassword());
 
 		User user = new User(
 				registerRequest.getName(),
 				registerRequest.getEmail(),
-				registerRequest.getPassword(),
+				encodedPassword,
 				registerRequest.getBirthDate(),
 				registerRequest.getGender(),
 				Role.USER,
