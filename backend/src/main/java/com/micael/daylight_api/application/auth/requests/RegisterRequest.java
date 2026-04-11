5a6235b9d4e47b3177bf205b952e060948fbff15
iSantanaaH <micaelsantana.dev@gmail.com>
2026-04-10 21:34:38 -0300

    use message in anntation NotBlank and annotation JsonFormat to define birthdate format

--- backend/src/main/java/com/micael/daylight_api/application/auth/requests/RegisterRequest.java
+++ backend/src/main/java/com/micael/daylight_api/application/auth/requests/RegisterRequest.java
@@ -1,14 +1,26 @@
 package com.micael.daylight_api.application.auth.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record RegisterRequest(
-        @NotBlank String name,
-        @NotBlank String email,
-        @NotBlank String password,
-        @NotBlank String phone,
-        @NotBlank LocalDate birthdate
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password,
+
+        @NotBlank(message = "Phone is required")
+        String phone,
+
+        @NotNull(message = "Birthdate is required")
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate birthdate
 ) {
 }
